d80b0729940297ac5823c2dded5a9da11280f415
NilsHeinzelmann <123253152+NilsHeinzelmann@users.noreply.github.com>
2026-04-08 20:44:44 +0200

    no second time qualification ending allowed
    no second time qualification ending allowed

--- TurnierManagerBackend/src/main/java/de/jf/karlsruhe/service/TournamentManagementService.java
+++ TurnierManagerBackend/src/main/java/de/jf/karlsruhe/service/TournamentManagementService.java
@@ -1,17 +1,22 @@
 package de.jf.karlsruhe.service;
 
+import de.jf.karlsruhe.model.base.Round;
 import de.jf.karlsruhe.model.base.Tournament;
 import de.jf.karlsruhe.model.dto.TournamentCreationDTO;
+import de.jf.karlsruhe.model.repos.RoundRepository;
 import de.jf.karlsruhe.model.repos.TournamentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
 
 @Service
 @RequiredArgsConstructor
 public class TournamentManagementService {
 
     private final TournamentRepository tournamentRepository;
+    private final RoundRepository roundRepository;
 
     /**
      * Erstellt eine Tournament-Entität aus dem DTO und speichert diese in der Datenbank.
@@ -41,5 +46,13 @@ public class TournamentManagementService {
         tournament.setPlayTimeInSeconds(playTimeInSeconds);
         tournament.setBreakTimeInSeconds(breakTimeInSeconds);
         tournamentRepository.save(tournament);
+    }
+
+        public Round getActiveRound() {
+            return roundRepository.findAll().stream()
+                    // Vergleicht alle Runden anhand ihres orderIndex
+                    .max(Comparator.comparingInt(Round::getOrderIndex))
+                    // Wirft eine Exception, falls noch gar keine Runden generiert wurden
+                    .orElseThrow(() -> new RuntimeException("Keine aktive Runde im System gefunden."));
     }
 }
